e476d03141a166805afb04333fa776bf30a34075
Luiz Guilherme Afonso de André <guilherme@MacBook-Air-de-Luiz.local>
2019-04-29 23:11:27 -0300

    Logo image + search input and basic css

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,15 +1,16 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-    * {
+    * { /* apply style to all elements */
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        outline: 0;
     }
 
     body {
         background: #9B65E6;
-        text-rendering: optimizeLegibility !important;
+        text-rendering: optimizeLegibility !important; /* makes text font more legible on chrome */
         -webkit-font-smoothing: antialiased !important;
     }
 `;
